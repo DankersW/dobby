a9c0f027febcc7b1acd0ea3a2224eb7163489217
DankersW <wouter.dankers@skynet.be>
2022-02-04 17:48:32 +0100

    Some cleanup

--- wsn_terminal/wsn_terminal.go
+++ wsn_terminal/wsn_terminal.go
@@ -1,15 +1,13 @@
 package wsn_terminal
 
 import (
-	"bufio"
 	"time"
 
 	log "github.com/sirupsen/logrus"
-	"github.com/tarm/serial"
 )
 
 type wsnTerminal struct {
-	port string
+	usbPort string
 	// TODO: handlers to call on a certain type of message
 }
 
@@ -19,31 +17,14 @@ type WsnTerminal interface {
 
 func New(port string) WsnTerminal {
 	wt := &wsnTerminal{
-		port: port,
+		usbPort: port,
 	}
 	return wt
 }
 
 func (wt *wsnTerminal) Start() {
-	/*
-		log.Info("starting")
-		port, err := term.Open("/dev/ttyACM0")
-		log.Info(err)
-		log.Info(port)
 
-		for {
-			var buff [100]byte
-			l, err := port.Read(buff[:])
-			if err == io.EOF {
-				log.Error("EOF")
-				break
-			}
-			log.Info(l)
-			log.Infof("%s", buff)
-		}
-	*/
-
-	serialTerm, err := newUartConnection("/dev/ttyACM1")
+	serialTerm, err := newUartConnection(wt.usbPort)
 	if err != nil {
 		log.Errorf("Failed to open Serial connection to WSN gateway, %s", err.Error())
 		return
@@ -51,7 +32,6 @@ func (wt *wsnTerminal) Start() {
 	serialTerm.setup()
 
 	count := 0
-
 	for {
 		data, err := serialTerm.read()
 		if err != nil {
@@ -67,50 +47,4 @@ func (wt *wsnTerminal) Start() {
 		}
 		count++
 	}
-
-	// FIXME: REMOVE
-
-	config := &serial.Config{
-		Name:        "/dev/ttyACM1",
-		Baud:        9600,
-		ReadTimeout: 1,
-		Size:        8,
-	}
-
-	stream, err := serial.OpenPort(config)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	//count := 0
-	for {
-		scanner := bufio.NewScanner(stream)
-		for scanner.Scan() {
-			log.Info(scanner.Text()) // Println will add back the final '\n'
-		}
-		if err := scanner.Err(); err != nil {
-			log.Fatal(err)
-		}
-
-		if count == 3 {
-			log.Info("c 20")
-
-			writer := bufio.NewWriter(stream)
-			writer.Reset(stream)
-			writer.WriteString("thread multi_light toggle\n")
-			writer.Flush()
-
-			count = 0
-		}
-		time.Sleep(1 * time.Second) // TODO: use a go-routine for this
-		count = count + 1
-	}
-
-	/*
-
-	 */
-
 }
-
-// Serial: https://pkg.go.dev/github.com/tarm/serial
-//https://stackoverflow.com/questions/50088669/golang-reading-from-serial
