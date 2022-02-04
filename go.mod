b0b5dcea69345ec3fbfcb3b84a14c2dc43bca6fa
DankersW <wouter.dankers@skynet.be>
2022-02-04 15:32:03 +0100

    serial: reading from serial device

--- go.mod
+++ go.mod
@@ -7,6 +7,7 @@ require (
 	github.com/google/gousb v2.1.0+incompatible // indirect
 	github.com/pkg/term v1.1.0
 	github.com/sirupsen/logrus v1.8.1
+	github.com/tarm/serial v0.0.0-20180830185346-98f6abe2eb07
 	google.golang.org/protobuf v1.26.0
 	gopkg.in/yaml.v3 v3.0.0-20210107192922-496545a6307b
 )
--- wsn_terminal/wsn_terminal.go
+++ wsn_terminal/wsn_terminal.go
@@ -1,10 +1,11 @@
 package wsn_terminal
 
 import (
-	"io"
+	"bufio"
+	"time"
 
-	"github.com/pkg/term"
 	log "github.com/sirupsen/logrus"
+	"github.com/tarm/serial"
 )
 
 type wsnTerminal struct {
@@ -24,21 +25,57 @@ func New(port string) WsnTerminal {
 }
 
 func (wt *wsnTerminal) Start() {
-	log.Info("starting")
-	port, err := term.Open("/dev/ttyACM0")
-	log.Info(err)
-	log.Info(port)
+	/*
+		log.Info("starting")
+		port, err := term.Open("/dev/ttyACM0")
+		log.Info(err)
+		log.Info(port)
 
-	for {
-		var buff [100]byte
-		l, err := port.Read(buff[:])
-		if err == io.EOF {
-			log.Error("EOF")
-			break
+		for {
+			var buff [100]byte
+			l, err := port.Read(buff[:])
+			if err == io.EOF {
+				log.Error("EOF")
+				break
+			}
+			log.Info(l)
+			log.Infof("%s", buff)
 		}
-		log.Info(l)
-		log.Infof("%s", buff)
+	*/
+
+	config := &serial.Config{
+		Name:        "/dev/ttyACM0",
+		Baud:        9600,
+		ReadTimeout: 1,
+		Size:        8,
 	}
+
+	stream, err := serial.OpenPort(config)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	count := 0
+	for {
+		scanner := bufio.NewScanner(stream)
+		for scanner.Scan() {
+			log.Info(scanner.Text()) // Println will add back the final '\n'
+		}
+		if err := scanner.Err(); err != nil {
+			log.Fatal(err)
+		}
+
+		if count == 5 {
+			log.Info("c 20")
+			count = 0
+		}
+		time.Sleep(1 * time.Second) // TODO: use a go-routine for this
+		count = count + 1
+	}
+
+	/*
+
+	 */
 
 }
 
